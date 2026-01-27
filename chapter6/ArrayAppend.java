d04e147c53fded30ab8052e2e5ff52d23fb7122a
sarahrae128-alt <sarahrae128@gmail.com>
2026-01-27 11:44:15 -0600

    Correct Code :)

--- chapter6/ArrayAppend.java
+++ chapter6/ArrayAppend.java
@@ -2,22 +2,23 @@ package chapter6;
 
 import java.util.ArrayList;
 
-public class ArrayAppend extends Target{
+public class ArrayAppend extends Target {
 
-    public ArrayAppend(String name, int[] arr, ArrayList<Integer> list) {
+    public ArrayAppend(int arr[], ArrayList<Integer> list, String name) {
         super(arr, list, name);
-    }
+    }    
 
     @Override
-    public int method(int[] indicesOrNums) {
+    public int method(int[] indicesOrnums) {
         int result = 0;
-        int largerarr[] = java.util.Arrays.copyOf(arr, arr.length + indicesOrNums.length);
-        for (int i = 0; i < indicesOrNums.length; i++) {
-            result += arr[indicesOrNums[i]];
-            largerarr[arr.length+i] = indicesOrNums[i];
+        int largerarr[] = java.util.Arrays.copyOf(arr, arr.length+indicesOrnums.length);
+        for (int i = 0; i < indicesOrnums.length; i++) {
+            result += arr[indicesOrnums[i]];
+            largerarr[arr.length+i] = indicesOrnums[i];
         }
         return result;
     }
+
+
+    
 }
-
-
